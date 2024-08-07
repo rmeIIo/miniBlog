2df1398d6a4da317ecf0977249508b6ec8f5fa97
rmeiio <j.rogercorreamello@gmail.com>
2024-08-07 11:07:44 -0300

    Adicionando componente de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // Pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
