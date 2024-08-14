25f14cc184cd6fd94d19377fd05b8bf0545584be
rmeiio <j.rogercorreamello@gmail.com>
2024-08-14 16:37:25 -0300

    Inserindo dados no firebase

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -3,6 +3,7 @@ import styles from "./CreatePost.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,30 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
+
+    // TODO: Validate image URL
+
+    // TODO: Criar o array de tags
+
+    // TODO: Checar todos os valores
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // TODO: redirect to homepage
   };
 
   return (
@@ -67,6 +90,13 @@ const CreatePost = () => {
           />
         </label>
 
+        {!response.loading && <button className="btn">Criar post!</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde... {response.loading}
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
       </form>
     </div>
   );
