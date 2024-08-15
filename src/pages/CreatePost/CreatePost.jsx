ba3616f7cde3ce00b900948f54645d91efc7a65d
rmeiio <j.rogercorreamello@gmail.com>
2024-08-15 15:21:54 -0300

    Validação dos dados

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -16,15 +16,25 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
-    // TODO: Validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
 
-    // TODO: Criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
-    // TODO: Checar todos os valores
+    if(!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos.");
+    }
+
+    if (formError) return;
 
     insertDocument({
       title,
@@ -36,6 +46,7 @@ const CreatePost = () => {
     });
 
     // TODO: redirect to homepage
+    navigate('/')
   };
 
   return (
@@ -93,10 +104,12 @@ const CreatePost = () => {
         {!response.loading && <button className="btn">Criar post!</button>}
         {response.loading && (
           <button className="btn" disabled>
-            Aguarde... {response.loading}
+            Aguarde.. .
           </button>
         )}
-        {response.error && <p className="error">{response.error}</p>}
+        {(response.error || formError) && (
+          <p className="error">{response.error || formError}</p>
+        )}
       </form>
     </div>
   );
