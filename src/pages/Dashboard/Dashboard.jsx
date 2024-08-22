9641996f1eb6f019a1a39956c19a00708c2a0148
rmeiio <j.rogercorreamello@gmail.com>
2024-08-22 07:24:39 -0300

    CSS da dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -32,12 +32,12 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
           {posts && posts.map((post) => (
-            <div key={post.id}>
+            <div key={post.id} className={styles.post_row}>
               <p>{post.title}</p>
               <div>
                 <Link to={`/posts/${post.id}`} className="btn btn-outline">
