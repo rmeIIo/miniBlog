44dcfe64717f4fd4c8c0cf10a9519fb6be683fdc
rmeiio <j.rogercorreamello@gmail.com>
2024-08-22 07:40:17 -0300

    Excluindo post

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 // hooks
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
@@ -13,10 +14,10 @@ const Dashboard = () => {
   // posts do usuário
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
-  const deleteDocument = (id) => {};
+  const { deleteDocument } = useDeleteDocument("posts");
 
-  if(loading) {
-    return <p>Carregando...</p>
+  if (loading) {
+    return <p>Carregando...</p>;
   }
 
   return (
@@ -36,28 +37,29 @@ const Dashboard = () => {
             <span>Título</span>
             <span>Ações</span>
           </div>
-          {posts && posts.map((post) => (
-            <div key={post.id} className={styles.post_row}>
-              <p>{post.title}</p>
-              <div>
-                <Link to={`/posts/${post.id}`} className="btn btn-outline">
-                  Ver
-                </Link>
-                <Link
-                  to={`/posts/edit/${post.id}`}
-                  className="btn btn-outline"
-                >
-                  Editar
-                </Link>
-                <button
-                  onClick={() => deleteDocument(post.id)}
-                  className="btn btn-outline btn-danger"
-                >
-                  Excluir
-                </button>
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </>
       )}
     </div>
