2b3f70cede4928979b58806b58a61f7d830eb2ac
rmeiio <j.rogercorreamello@gmail.com>
2024-08-22 08:03:29 -0300

    Atualizando post

--- src/pages/EditPost/EditPost.jsx
+++ src/pages/EditPost/EditPost.jsx
@@ -3,8 +3,8 @@ import styles from "./EditPost.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
-import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
 
 const EditPost = () => {
   const { id } = useParams();
@@ -30,7 +30,7 @@ const EditPost = () => {
 
   const { user } = useAuthValue();
 
-  const { insertDocument, response } = useInsertDocument("posts");
+  const { updateDocument, response } = useUpdateDocument("posts");
 
   const navigate = useNavigate();
 
@@ -52,16 +52,18 @@ const EditPost = () => {
 
     if (formError) return;
 
-    insertDocument({
+    const data = {
       title,
       image,
       body,
       tags: tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
-    });
+    };
 
-    navigate("/");
+    updateDocument(id, data);
+
+    navigate("/dashboard");
   };
 
   return (
